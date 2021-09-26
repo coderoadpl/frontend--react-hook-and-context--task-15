187084d95a871a65ac931d70a603c9d819eca35d
Mateusz Choma <kontakt@coderoad.pl>
2021-09-26 16:13:39 +0200

    Fetching data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react'
 
+const URL = 'https://randomuser.me/api?results=10'
+
 export const App = () => {
+  const [data, setData] = React.useState(null)
+
+  React.useEffect(() => {
+    fetch(URL)
+      .then((r) => r.json())
+      .then((responseData) => setData(responseData.results))
+  }, [])
+
   return (
-    <div>
-      CodeRoad APP
-    </div>
+    <ul>
+      {
+        data === null ?
+          'No data'
+          :
+          data.map((user) => {
+            return (
+              <li key={user.email}>
+                {user.email}
+              </li>
+            )
+          })
+      }
+    </ul>
   )
 }
 
