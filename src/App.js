383cf560386d8833910a7328f8c5db8a082bad26
Mateusz Choma <kontakt@coderoad.pl>
2021-09-26 16:17:49 +0200

    Adding loading states

--- src/App.js
+++ src/App.js
@@ -4,26 +4,37 @@ const URL = 'https://randomuser.me/api?results=10'
 
 export const App = () => {
   const [data, setData] = React.useState(null)
+  const [isLoading, setIsLoading] = React.useState(true)
+  const [hasError, setHasError] = React.useState(false)
 
   React.useEffect(() => {
+    setIsLoading(true)
     fetch(URL)
       .then((r) => r.json())
       .then((responseData) => setData(responseData.results))
+      .catch((error) => setHasError(error))
+      .finally(() => setIsLoading(false))
   }, [])
 
   return (
     <ul>
       {
-        data === null ?
-          'No data'
-          :
-          data.map((user) => {
-            return (
-              <li key={user.email}>
-                {user.email}
-              </li>
-            )
-          })
+       hasError ?
+         'Error!'
+         :
+         isLoading ?
+           'Loading'
+           :
+             !Array.isArray(data) ?
+               'No data'
+               :
+               data.map((user) => {
+                 return (
+                   <li key={user.email}>
+                     {user.email}
+                   </li>
+                 )
+               })
       }
     </ul>
   )
